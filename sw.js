ab162080e87a11b41e64adc1cf867bc04fb33a5e
sleepingjotun <128058989+sleepingjotun@users.noreply.github.com>
2026-04-07 15:09:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,6 @@
-const CACHE = 'piano-v6';
-const FILES = ['/piano-alimentare/', '/piano-alimentare/index.html', '/piano-alimentare/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
-  );
+// Questo service worker si auto-rimuove
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+  self.registration.unregister();
+  caches.keys().then(keys => keys.forEach(k => caches.delete(k)));
 });
